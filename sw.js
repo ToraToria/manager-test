fba8aab5ebbe37ae79d96d529da6c3b0795b52a0
toratoria <vikalas57@gmail.com>
2025-11-26 15:04:59 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// sw.js - Service Worker
+const CACHE_NAME = 'manager-test-v1.3';
+const urlsToCache = [
+  '/manager-test/',
+  '/manager-test/index.html',
+  '/manager-test/test.html',
+  '/manager-test/results.html',
+  '/manager-test/styles.css',
+  '/manager-test/main.js',
+  '/manager-test/user.js',
+  '/manager-test/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
