03c53334bd0611ab7a6516f61b693dd3a6b08939
toratoria <vikalas57@gmail.com>
2025-12-11 22:17:01 +0300

    Update user.js

--- user.js
+++ user.js
@@ -8,6 +8,16 @@ function startTest() {
     return;
   }
 
+  // Проверка минимальной длины
+  if (firstName.length < 2 || lastName.length < 2) {
+    alert('Имя и фамилия должны содержать минимум 2 символа.');
+    return;
+  }
+
+  // Очищаем предыдущие результаты
+  localStorage.removeItem('lastScore');
+  localStorage.removeItem('lastLevel');
+
   localStorage.setItem('firstName', firstName);
   localStorage.setItem('lastName', lastName);
 
