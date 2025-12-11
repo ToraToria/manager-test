6015c0ea735b8edf80fcec2eb97c016dce95b556
toratoria <vikalas57@gmail.com>
2025-12-11 22:13:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker
-const CACHE_NAME = 'manager-test-v1.5';
+const CACHE_NAME = 'manager-test-v2.0';
 const urlsToCache = [
   '/manager-test/',
   '/manager-test/index.html',
@@ -8,19 +8,55 @@ const urlsToCache = [
   '/manager-test/styles.css',
   '/manager-test/main.js',
   '/manager-test/user.js',
-  '/manager-test/manifest.json'
+  '/manager-test/manifest.json',
+  '/manager-test/icon-192.png',
+  '/manager-test/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
   );
 });
